05225d989a816b543834c3c551c36fb14c0b2f27
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-13 21:59:25 -0500

    Added doc comment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
+//! Raw definitions imported from the C library (via bindgen).
+//!
+//! This module is generated and undocumented, but you can see the [C
+//! libffi documentation](libffi.txt).
+
 mod generated;
 pub use generated::*;
