335cf87ac5ef7f12a838749c6c7341350eca7ebc
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-20 23:05:12 -0500

    Builds its own libffi now.

--- build.rs
+++ build.rs
@@ -1,42 +1,79 @@
 extern crate bindgen;
-extern crate pkg_config;
+extern crate curl;
 
+use std::env;
+use std::fs::File;
+use std::io::Write;
 use std::path::Path;
+use std::process::Command;
+
+use curl::easy::Easy;
+
+const LIBFFI_ARCHIVE: &'static str = "libffi.tar.gz";
+const LIBFFI_DIR: &'static str     = "libffi-3.2.1";
+const LIBFFI_URL: &'static str     =
+    "ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz";
 
 fn main() {
-    let include_path = Path::new("include").join("include_ffi.h");
-    let out_path = Path::new("src").join("generated.rs");
+    let out_dir   = env::var("OUT_DIR").unwrap();
 
-    let libffi = pkg_config::probe_library("libffi").expect("
-        **********
-        pkg-config could not find libffi. This could be because you
-        don't have pkg-config, because you don't have libffi, or because
-        they don't know about each other. If you can run `pkg-config
-        libffi --cflags` and get a reasonable result, please file a bug
-        report.
-        **********
-    ");
+    let archive   = Path::new(&out_dir).join(LIBFFI_ARCHIVE);
+    let build_dir = Path::new(&out_dir).join(LIBFFI_DIR);
+    let prefix    = Path::new(&out_dir).join("libffi-root");
+    let include   = Path::new(&prefix).join("lib")
+                                      .join("libffi-3.2.1")
+                                      .join("include");
+    let libdir    = Path::new(&prefix).join("lib");
+
+    // Download libffi
+    let mut file  = File::create(&archive).unwrap();
+    let mut curl = Easy::new();
+    curl.url(LIBFFI_URL).unwrap();
+    curl.write_function(move |data| Ok(file.write(data).unwrap())).unwrap();
+    curl.perform().unwrap();
+
+    fn run_command(which: &'static str, cmd: &mut Command) {
+        assert!(cmd.status().expect(which).success(), which);
+    }
+
+    // Untar it.
+    run_command("Untarring libffi",
+                Command::new("tar")
+                        .arg("-zxf")
+                        .arg(LIBFFI_ARCHIVE)
+                        .current_dir(&out_dir));
+
+    // Configure, make, make install
+    run_command("Configuring libffi",
+                Command::new("./configure")
+                        .arg("--disable-shared")
+                        .arg("--prefix")
+                        .arg(prefix)
+                        .current_dir(&build_dir));
+    run_command("Building libffi",
+                Command::new("make")
+                        .arg("install")
+                        .current_dir(&build_dir));
+
+    // Now run bindgen.
+
+    let include_file = Path::new("include").join("include_ffi.h");
+    let out_file = Path::new("src").join("generated.rs");
 
     let mut builder = bindgen::Builder::default();
-    builder.header(include_path.display().to_string());
-    for path in &libffi.include_paths {
-        builder.clang_arg(format!("-I{}", path.display()));
-    }
+    builder.header(include_file.display().to_string());
+    builder.clang_arg(format!("-I{}", include.display()));
 
     let bindings = builder.generate().expect("
         **********
         Bindgen generation failed. Please file a bug report.
         **********
     ");
-    bindings.write_to_file(out_path.display().to_string())
+    bindings.write_to_file(out_file.display().to_string())
             .expect("bindgen output");
 
-    for lib in &libffi.libs {
-        println!("cargo:rustc-link-lib={}", lib);
-    }
-
-    for path in &libffi.link_paths {
-        println!("cargo:rustc-link-search={}", path.display());
-    }
+    // Cargo linking directives
+    println!("cargo:rustc-link-lib=libffi");
+    println!("cargo:rustc-link-search={}", libdir.display());
 }
 
